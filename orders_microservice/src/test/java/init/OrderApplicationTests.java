76d5d89791cb1b63a36959b7df9d92dd8d6a5822
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-07-17 13:55:43 +0200

    test unitarios de modulos

---
+++ orders_microservice/src/test/java/init/OrderApplicationTests.java
@@ -0,0 +1,18 @@
+package init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = OrderApplication.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost", "port=9092" },
+        topics = "topicOrders")
+@ActiveProfiles("test")
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() { }
+}
